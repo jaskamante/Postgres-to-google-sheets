1759eb8eed191e53644be70310418af873c17f00
Jasmine Kamante <32465451+jaskamante@users.noreply.github.com>
2019-04-13 22:26:16 +1200

    Delete dummy_config.py
    replace with more dummy info

--- dummy_config.py
+++
@@ -1,7 +0,0 @@
-# Add your config info into this file and rename to config.py
-
-CONFIG_INFO = {
-    'table_name': 'vmm_zra_csv_3_branch',
-    'sheet_name': 'PY to Gsheet Test',
-    'credential_file_name': 'client_secret.json'
-}
