cae785c41afab54c3a8398d8849ae0dd61bf4df7
Data Agency <dataagency@Jasmines-MacBook.local>
2019-04-13 22:27:57 +1200

    added code and instructions for transferring data to google sheets

--- dummy_config.py
+++ dummy_config.py
@@ -1,7 +1,7 @@
 # Add your config info into this file and rename to config.py
 
 CONFIG_INFO = {
-    'table_name': 'vmm_zra_csv_3_branch',
-    'sheet_name': 'PY to Gsheet Test',
+    'table_name': 'your table name',
+    'sheet_name': 'your sheet name',
     'credential_file_name': 'client_secret.json'
 }
