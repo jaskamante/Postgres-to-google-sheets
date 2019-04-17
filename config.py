97c1af06d81f70026410702d9dfbd189736349fa
jaskamante <jaskamante@gmail.com>
2019-04-17 19:06:12 +1200

    refactored code to use a single config.json

---
+++ config.py
@@ -0,0 +1,14 @@
+import json
+
+with open('config.json') as config_file:
+    data = json.load(config_file)
+
+#google sheets details
+
+def config():
+
+    config_data = {}
+    config_data['table_name'] = data['gsheets']['table_name']
+    config_data['sheet_name'] = data['gsheets']['sheet_name']
+    config_data['credential_file_name'] = data['gsheets']['credential_file_name']
+    return config_data
