a6e12f68bb0fb80d0c82f30f19367d92d418d673
jaskamante <jaskamante@gmail.com>
2019-04-17 19:13:03 +1200

    refactored code to use a single config.json

--- dummy_config.py
+++
@@ -1,7 +0,0 @@
-# Add your config info into this file and rename to config.py
-
-CONFIG_INFO = {
-    'table_name': 'your table name',
-    'sheet_name': 'your sheet name',
-    'credential_file_name': 'client_secret.json'
-}
